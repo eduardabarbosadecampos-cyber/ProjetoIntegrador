6991b9abc11c2fd22db443b2cd4aa18c1078e384
Eduarda <eduardabarbosadecampos@gmail.com>
2026-06-02 21:14:14 -0300

    mensagem

---
+++ Jogo da Memória/Jogo da Memória/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Jogo_da_Memória
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
