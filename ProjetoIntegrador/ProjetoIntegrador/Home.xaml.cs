2f7b1b69ec24d6a2820cb812c0eda6de552407a5
Eduarda <eduardabarbosadecampos@gmail.com>
2026-06-03 21:46:29 -0300

    Commit

--- ProjetoIntegrador/ProjetoIntegrador/Home.xaml.cs
+++ ProjetoIntegrador/ProjetoIntegrador/Home.xaml.cs
@@ -57,3 +57,18 @@ namespace ProjetoIntegrador
         }
     }
 }
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string CPF { get; set; }
+
+    public string Telefone { get; set; }
+
+    public string Cidade { get; set; }
+
+    public decimal TotalCompras { get; set; }
+}
